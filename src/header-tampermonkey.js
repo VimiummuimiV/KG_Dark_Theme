6f9b6f82dda26309511813dce6fe986f1212767e
Puncher <puncher@gmail.com>
2025-07-16 04:50:45 +0300

    Smiles integration. No need anymore for separate stylish code.

--- src/header-tampermonkey.js
+++ src/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.2
+// @version        1.0.3
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
