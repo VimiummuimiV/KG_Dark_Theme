583052bd5c85eb48643a33b9169a4bdade09e035
Puncher <puncher@gmail.com>
2025-07-18 01:14:04 +0300

    userPanel.scss fix #1

--- src/header-tampermonkey.js
+++ src/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.5
+// @version        1.0.6
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
