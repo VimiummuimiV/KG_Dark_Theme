0cac38c876b81a2234c67ce7472bcf2b066f55cc
Puncher <puncher@gmail.com>
2025-07-18 07:06:32 +0300

    NEW Ru and En keyboards

--- src/header-stylish.js
+++ src/header-stylish.js
@@ -1,7 +1,7 @@
 /* ==UserStyle==
 @name           KG_Dark
 @namespace      klavogonki.ru
-@version        1.0.6
+@version        1.0.7
 @description    Dark theme for klavogonki.ru is made to save your eyes from the bright background.
 @author         Patcher
 ==/UserStyle== */
