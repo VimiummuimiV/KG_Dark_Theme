490313b48f62b71443fdad49e30451f668b3c468
Puncher <puncher@gmail.com>
2025-07-20 01:53:50 +0300

    adaptive logo

--- src/header-stylish.js
+++ src/header-stylish.js
@@ -1,7 +1,7 @@
 /* ==UserStyle==
 @name           KG_Dark
 @namespace      klavogonki.ru
-@version        1.0.9
+@version        1.0.10
 @description    Dark theme for klavogonki.ru is made to save your eyes from the bright background.
 @author         Patcher
 ==/UserStyle== */
--- src/header-tampermonkey.js
+++ src/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.9
+// @version        1.0.10
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
