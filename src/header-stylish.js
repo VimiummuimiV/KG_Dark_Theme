b7672925314b8659b12fc63d1e26728e9c0280ca
Puncher <puncher@gmail.com>
2025-07-21 22:52:03 +0300

    Update car SVG definitions and version numbers for dark theme
    - Added a new car SVG definition for car10 in _defaults.scss.
    - Commented out the previous car10 image URL in _defaults.scss.
    - Updated version number to 1.0.12 in header-stylish.js and header-tampermonkey.js.

--- src/header-stylish.js
+++ src/header-stylish.js
@@ -1,7 +1,7 @@
 /* ==UserStyle==
 @name           KG_Dark
 @namespace      klavogonki.ru
-@version        1.0.11
+@version        1.0.12
 @description    Dark theme for klavogonki.ru is made to save your eyes from the bright background.
 @author         Patcher
 ==/UserStyle== */
