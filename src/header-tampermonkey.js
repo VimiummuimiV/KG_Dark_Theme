08408498724ebd09069fb56ad18f5772bdaec8fc
Puncher <puncher@gmail.com>
2025-07-28 02:40:23 +0300

    Update styles and icons for vocabulary page, enhance floating navigation, and increment version numbers
    - Added new styles for the vocabulary page comments block, including layout adjustments and icon usage.
    - Removed unnecessary justification from the floating navigation menu.
    - Updated version numbers in user styles for both Tampermonkey and Stylish scripts.
    - Imported new vocabulary styles into the main stylesheet.

--- src/header-tampermonkey.js
+++ src/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.19
+// @version        1.0.20
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
