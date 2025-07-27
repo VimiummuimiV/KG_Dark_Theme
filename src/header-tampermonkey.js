f4ff67f3756a775822a2f7dd4b435b20212dfaec
Puncher <puncher@gmail.com>
2025-07-27 05:38:27 +0300

    Full restyle and selectors order review. Also added better adaptivity for vocabularies.scss.

--- src/header-tampermonkey.js
+++ src/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.18
+// @version        1.0.19
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
