0f43ecaa94a20dfd018e59959e33ef26c4218c2a
Puncher <puncher@gmail.com>
2025-07-27 03:39:48 +0300

    All cars tuning fixed from white spots and lines.

--- src/header-stylish.js
+++ src/header-stylish.js
@@ -1,7 +1,7 @@
 /* ==UserStyle==
 @name           KG_Dark
 @namespace      klavogonki.ru
-@version        1.0.17
+@version        1.0.18
 @description    Dark theme for klavogonki.ru is made to save your eyes from the bright background.
 @author         Patcher
 ==/UserStyle== */
