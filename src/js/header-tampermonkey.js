690624f912f31ae9bd37fa0e1ab5105781711f45
Puncher <puncher@gmail.com>
2025-07-30 15:59:24 +0300

    Better vocabulary comments design

--- src/js/header-tampermonkey.js
+++ src/js/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.26
+// @version        1.0.27
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
