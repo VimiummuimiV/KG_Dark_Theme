da7486fd8e900f6be0fc897ddb31e21130e3fcfb
Puncher <puncher@gmail.com>
2025-07-30 09:46:21 +0300

    Ensure that the vocabulary comments and forum comments have a similar appearance.

--- src/js/header-tampermonkey.js
+++ src/js/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.24
+// @version        1.0.25
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
