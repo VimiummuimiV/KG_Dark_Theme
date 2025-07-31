06f979a660eb32492c247bec95e7cb34661e306f
Puncher <puncher@gmail.com>
2025-07-31 23:40:57 +0300

    Version bump. 1.0.30

--- dist/KG_Dark_Theme.js
+++ dist/KG_Dark_Theme.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.29
+// @version        1.0.30
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
