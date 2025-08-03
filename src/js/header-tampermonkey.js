64c55d10462d85cbcaa7a2576f0be617080742ad
Puncher <puncher@gmail.com>
2025-08-03 14:35:30 +0300

    Fix for header.

--- src/js/header-tampermonkey.js
+++ src/js/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.33
+// @version        1.0.34
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
