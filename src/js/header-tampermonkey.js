e67a4c54620a1ed0f360cdb6463b7a0b94b73795
Puncher <puncher@gmail.com>
2025-08-05 12:56:48 +0300

    padding: 5px for .item when adaptive

--- src/js/header-tampermonkey.js
+++ src/js/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.39
+// @version        1.0.40
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
