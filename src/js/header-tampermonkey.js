9f88d80e40bee48a5d96662e015aa9ca1cb307ce
Puncher <puncher@gmail.com>
2025-08-07 16:14:50 +0300

    vocabulary comments limited and expanded view into one file vocabulary-comments.scss

--- src/js/header-tampermonkey.js
+++ src/js/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.45
+// @version        1.0.46
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
