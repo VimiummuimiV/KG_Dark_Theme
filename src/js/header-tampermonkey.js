29d7361f67c670da92d2785681a4874ca92faa8d
Puncher <puncher@gmail.com>
2025-08-08 00:49:59 +0300

    chat.scss disable gamelist when chat is opened on viewport width less than 800px

--- src/js/header-tampermonkey.js
+++ src/js/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.46
+// @version        1.0.47
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
