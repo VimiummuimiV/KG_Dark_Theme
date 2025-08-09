e3141beb688f55d5a424d67495647fdde37d3a68
Puncher <puncher@gmail.com>
2025-08-10 01:12:47 +0300

    Enhance responsive design for posts list and update dark theme script version
    - Added media queries to improve layout on smaller screens in _postsList.scss
    - Adjusted flex properties for better alignment and spacing in post elements
    - Updated header-tampermonkey.js to version 1.0.55

--- src/js/header-tampermonkey.js
+++ src/js/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.54
+// @version        1.0.55
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
