b422b9ec0c727d03025ab9b25459b8d196d25e03
Puncher <puncher@gmail.com>
2025-08-09 22:17:23 +0300

    Refactor styles for .write elements and enhance moderators block styling
    - Updated the SCSS for .write elements to use :has() for better specificity and added common styles.
    - Improved the styling for the moderators block, including new layout and hover effects for user links.
    - Incremented the version of the Tampermonkey script to 1.0.54.
    - Adjusted the anchor-to-button mixin to use a more standard font size.

--- src/js/header-tampermonkey.js
+++ src/js/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.53
+// @version        1.0.54
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
