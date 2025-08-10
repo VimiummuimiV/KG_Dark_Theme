9dcb1cb4de8ec9e8f4082b972bf620deebccb478
Puncher <puncher@gmail.com>
2025-08-10 16:04:31 +0300

    chore: update version number to 1.0.56 in header tampermonkey script

--- src/js/header-tampermonkey.js
+++ src/js/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.55
+// @version        1.0.56
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
