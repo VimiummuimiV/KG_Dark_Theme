e048827427d59ef7cfe37235c03f86e3e3888f84
Puncher <puncher@gmail.com>
2025-08-16 00:44:37 +0300

    Update formatButtons.scss for mobile responsiveness and adjust icon color
    - Removed default icon color variable and replaced it with a direct reference to $lightblue.
    - Added mobile responsive styles to handle button display on screens smaller than 460px.
    - Implemented a fake button using the ::before pseudo-element to enhance user experience on mobile.
    - Adjusted opacity and pointer events for actual buttons to show/hide appropriately on hover.
    - Updated header-tampermonkey.js version to 1.0.63.

--- src/js/header-tampermonkey.js
+++ src/js/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.62
+// @version        1.0.63
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
