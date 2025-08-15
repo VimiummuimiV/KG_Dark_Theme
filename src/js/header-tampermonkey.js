30db7ca07b3bab49a5648b4fb4c5dfbb59d7f6a7
Puncher <puncher@gmail.com>
2025-08-15 21:45:16 +0300

    Add text formatting icons in SVG format to _textFormatIcons.scss

--- src/js/header-tampermonkey.js
+++ src/js/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.61
+// @version        1.0.62
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
