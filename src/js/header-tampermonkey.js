01f9c2990135497f3090e29f3fa82d44571f1f81
Puncher <puncher@gmail.com>
2025-08-17 00:37:24 +0300

    Update KG_Dark_Theme to version 1.0.64 and refactor responsive styles in toplist.scss
    - Bump version number in header-tampermonkey.js to 1.0.64.
    - Remove outdated media queries and styles for small screens in toplist.scss.
    - Consolidate adaptive styles for mobile and tablet into a single media query.
    - Enhance layout adjustments for #toplist and its child elements for better responsiveness.

--- src/js/header-tampermonkey.js
+++ src/js/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.63
+// @version        1.0.64
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
