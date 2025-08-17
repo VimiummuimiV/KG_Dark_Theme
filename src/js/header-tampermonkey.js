7c4b5f2ddf5edbbe64ee0d000c6c5d1df7ccd59f
Puncher <puncher@gmail.com>
2025-08-17 22:15:16 +0300

    Header blocks correct position when user not authorized (top 0)

--- src/js/header-tampermonkey.js
+++ src/js/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.65
+// @version        1.0.66
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
