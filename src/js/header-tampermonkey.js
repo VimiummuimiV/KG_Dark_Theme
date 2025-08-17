ddb4e91a038106fe9b2c48d16ec811d95fd0b68b
Puncher <puncher@gmail.com>
2025-08-18 01:30:00 +0300

    Update styles and icons in various components
    - Changed button font-weight to 700 and updated font-family to Montserrat in base.scss.
    - Added a new logOut icon SVG to elementIcons.scss.
    - Incremented version number in header-tampermonkey.js from 1.0.67 to 1.0.68.
    - Removed lighter font-weight from bold elements in modalDialogue.scss.
    - Modified contact button styles in messages.scss to include border and border-radius.
    - Refactored icon styles in userPanel.scss for better readability and added hover effects for logout buttons.

--- src/js/header-tampermonkey.js
+++ src/js/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.67
+// @version        1.0.68
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
