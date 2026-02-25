e4906c4b75b8d84f365340e25f9b625fe9c21672
Aboba <blahblahmail.com>
2026-02-25 18:21:35 +0200

    #topic-title-edit-block restyle.

--- src/js/header-tampermonkey.js
+++ src/js/header-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KG_Dark_Theme
 // @namespace      klavogonki
-// @version        1.0.74
+// @version        1.0.75
 // @description    Load dark theme CSS
 // @author         Patcher
 // @match          *://klavogonki.ru/*
